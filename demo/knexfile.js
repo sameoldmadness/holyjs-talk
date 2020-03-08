72c569c967065c54cc936d64bc49b6ee4672b85b
Roman Paradeev <roman.paradeev@klarna.com>
2020-03-08 22:06:59 +0100

    update pg creds

--- demo/knexfile.js
+++ demo/knexfile.js
@@ -1,5 +1,5 @@
 module.exports = {
   client: "pg",
-  connection: 'postgresql://postgres@localhost:5555/postgres',
+  connection: 'postgresql://postgres:postgres@localhost:5555/postgres',
   debug: true,
 };
